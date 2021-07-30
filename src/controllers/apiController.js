065ab348cdbe339c82baac60801cae2af1e18857
agustinpittari <agustinpittari4@hotmail.com>
2021-07-30 17:22:11 -0300

    update en proceso

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -7,32 +7,26 @@ module.exports = {
     },
     create: async (req, res) => {
         let data = req.body
-        let aprox_distance = geolib.getDistance(
-            {lat:Number(data.origin_lat), lng:Number(data.origin_long)},
-            {lat:Number(data.end_lat), lng:Number(data.end_long)})
+        let aprox_distance = geolib.getDistance({lat:Number(data.origin_lat), lng:Number(data.origin_long)},{lat:Number(data.end_lat), lng:Number(data.end_long)}) / 1000
         let newShipping = Shipping.create({
             ...req.body,
             current_lat: data.origin_lat,
             current_long: data.origin_long,
             aprox_distance
         })
-        res.json({
-            meta:{
-                status: 200,
-                message: 'Ok'
-            },
-            data: {
-                ...data,
-                aprox_distance
-            }
-        })
+        res.status(200).json({message: 'succesfull created'})
     },
     update: async (req, res) => {
         let data = req.body
-        // let updateShipping = Shipping.update({
-        //     ...req.body,
-        //     aprox_distance
-        // },
-        // {where: {id:req.params.id}})
+        let shippin = await Shipping.findByPk(req.params.id)
+        let aprox_distance = geolib.getDistance({lat:Number(data.current_lat), lng:Number(data.current_long)},{lat:Number(shippin.end_lat), lng:Number(shippin.end_long)}) / 1000
+        
+        let updateShipping = await Shipping.update({
+            ...req.body,
+            aprox_distance
+        },
+        {where: {id:req.params.id}})
+        res.json(updateShipping)
+
     }
 }
